edc642346a3040bf0a6828f6f793ee8cd723992b
Gabriel <gabrieldubois.eng@gmail.com>
2022-05-17 16:43:27 -0400

    huh

--- Driver.java
+++ Driver.java
@@ -1,46 +1,33 @@
 import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
 
 public class Driver {
     public static void main(String[] args) {
 
-    LocalDate rDate[] = {LocalDate.parse("2014-02-14"), LocalDate.parse("2002-01-23") };
-    int rSlot[] = {1, 2};
-    String rMedicare[] = {"EEE11","YYY99"};
- 
-}
-    
-
-
-public int rearrangeReservations(LocalDate[] date, int[] slot, String[] medicare){
-    LocalDate today = LocalDate.now();
-    int counter = 0;
-    for (int i = 0; i < date.length; i++){
+        LocalDate rDate[] = { LocalDate.parse("2014-02-14"), LocalDate.parse("2002-01-23") };
+        int rSlot[] = { 1, 2 };
+        String rMedicare[] = { "EEE11", "YYY99" };
 
     }
-    return counter;
-}
-public static void bubbleSort(LocalDate [] date_arr, int [] slot_arr, String [] medi_arr){
-        
-    for (int i=0;i<(date_arr.length)-1;++i){
-    
-        for(int j=0;j<(date_arr.length)-i-1; ++j){
-    
-            if(date_arr[j+1].compareTo(date_arr[j]) < 1 ){
-    
-                LocalDate swap = date_arr[j];
-                date_arr[j] = date_arr[j+1];
-                date_arr[j+1] = swap;
 
-                int swup = slot_arr[j];
-                slot_arr[j] = slot_arr[j+1];
-                slot_arr[j+1] = swup;
+    public int rearrangeReservations(LocalDate[] date, int[] slot, String[] medicare) {
+        LocalDate today = LocalDate.now();
+        int counter = 0;
+        for (int i = 0; i < date.length; i++) {
 
-                String swop = medi_arr[j];
-                medi_arr[j] = medi_arr[j+1];
-                medi_arr[j+1] = swop;
+        }
+        return counter;
+    }
+
+    public static void sort(LocalDate[] date_arr, int[] slot_arr, String[] medi_arr) {
+        LocalDate today = LocalDate.now();
+        // Try to make old reservation last and new first
+        for (int i = 0; i < (date_arr.length) - 1; ++i) {
+
+            if (date_arr[i].compareTo(today) < 1) {
+
             }
         }
     }
-}
 
 }
--- Pseudo-code.md
+++ Pseudo-code.md
@@ -1,3 +1,23 @@
 # Programming Questions - Pseudo Code
 
     rearrangeReservations(rDate[], rSlot[], rMedicare[])
+
+     for (int i = 0; i < (date_arr.length) - 1; ++i) {
+
+            for (int j = 0; j < (date_arr.length) - i - 1; ++j) {
+
+                if (date_arr[j + 1].compareTo(date_arr[j]) < 1) {
+
+                    LocalDate swap = date_arr[j];
+                    date_arr[j] = date_arr[j + 1];
+                    date_arr[j + 1] = swap;
+
+                    int swup = slot_arr[j];
+                    slot_arr[j] = slot_arr[j + 1];
+                    slot_arr[j + 1] = swup;
+
+                    String swop = medi_arr[j];
+                    medi_arr[j] = medi_arr[j + 1];
+                    medi_arr[j + 1] = swop;
+                }
+            }
