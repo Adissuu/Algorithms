06f71371cee2599adf92d2315cd4354cc0a3e63d
Gabriel <gabrieldubois.eng@gmail.com>
2022-05-18 12:59:34 -0400

    yeaaaaaaaah

--- Driver.java
+++ Driver.java
@@ -1,10 +1,8 @@
-import javax.naming.ldap.Rdn;
-
 public class Driver {
     public static void main(String[] args) {
 
        
-        Date rDate[] = { new Date("2021-11-25"), new Date("2022-03-08"), new Date("2021-12-02"), new Date("2022-02-03"), new Date("2021-12-02"), new Date("2021-12-03"), new Date("2022-01-25"), new Date("2022-01-14"), new Date("2022-02-01"), new Date("2022-01-01") };
+        Date rDate[] = { new Date("2021-11-25"), new Date("2022-03-08"), new Date("2021-12-02"), new Date("2022-02-01"), new Date("2021-12-02"), new Date("2021-12-03"), new Date("2022-01-25"), new Date("2022-01-14"), new Date("2022-02-01"), new Date("2022-01-01") };
         int rSlot[] = { 25, 10, 7, 15, 3, 40, 36, 11, 10, 15};
         String rMedicare[] = { "EEE11", "YYY99", "A","A","A","A","A","A", "a","a" };
         Date today = new Date("2022-01-01");
@@ -18,7 +16,7 @@ public class Driver {
         
         for (int i = 0; i < n - 1; ++i) {
             if(n == 1) break;
-            if ((date[i + 1].number < date[i].number)) {
+            if ((date[i + 1].number < date[i].number) || ((date[i+1].number == date[i].number) && (slot[i+1] < slot[i]))) {
                 //bubble sort
                 Date swap = date[i];
                 date[i] = date[i + 1];
@@ -32,22 +30,6 @@ public class Driver {
                 medicare[i] = medicare[i + 1];
                 medicare[i + 1] = swop; 
                 //check slot
-            
-            // if(date[i+i].number == date[i].number && slot[i+1] < slot[i]){
-            //     if(slot[i+1] < slot[i]){
-            //      swap = date[i];
-            //     date[i] = date[i + 1];
-            //     date[i + 1] = swap;
-    
-            //      swup = slot[i];
-            //     slot[i] = slot[i + 1];
-            //     slot[i + 1] = swup;
-    
-            //      swop = medicare[i];
-            //     medicare[i] = medicare[i + 1];
-            //     medicare[i + 1] = swop; 
-            //     }         
-            // }
         }
         rearrangeReservations(date, slot, medicare, today, n-1);
     }
@@ -55,7 +37,7 @@ public class Driver {
         int tSlot[] = new int[date.length];
         String tMedicare[] = new String[date.length];
         int left,right, active = 0;
-        for (int i = 0;i < date.length-1;i++){
+        for (int i = 0;i < date.length;i++){
             left = 0;
             right = date.length-1;
             if (date[i].number > today.number){
@@ -69,6 +51,7 @@ public class Driver {
             else{
                 while (tDate[right] != null)
                     right--;
+            
                 tDate[right] = date[i];
                 tSlot[right] = slot[i];
                 tMedicare[right] = medicare[i];
@@ -79,7 +62,7 @@ public class Driver {
             slot[i] = tSlot[i];
             medicare[i] = tMedicare[i];
          }
-        return 1;
+        return active;
     }
 
 }
