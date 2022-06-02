e2e0ab5699400595835022ca7f2cc44595364f92
Fetyali7 <70493307+Fetyali7@users.noreply.github.com>
2022-06-02 18:02:30 -0400

    Add files via upload

---
+++ Assignment_2/A2/MyContainer.java
@@ -0,0 +1,21 @@
+public interface MyContainer<T extends Comparable>
+{
+    
+    public void insert(T obj);
+
+    
+    public int getSize();
+
+
+    public boolean isEmpty();
+
+    
+    public T remove(int remIdx);
+
+    public T get(int idx);
+
+   
+    public T min();
+     
+    public T removeMin();
+}
