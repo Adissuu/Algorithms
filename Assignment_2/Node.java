86ab742c18eab8141ceb93389dc97b44d68df587
Fetyali7 <70493307+Fetyali7@users.noreply.github.com>
2022-06-04 18:53:50 -0400

    Update Node.java

--- Assignment_2/Node.java
+++ Assignment_2/Node.java
@@ -1,19 +1,22 @@
-public class Node<K extends Comparable<K>, V> {
+import java.util.Comparator;
+  class Node <K extends Comparable<K>, V> implements Comparator<Node<K, V>> {
 
-    private K key;
-    private V value;
-
+    K key;
+    V value;
+    Node <K,V> prev;
+    Node <K,V> next;
+  
     public Node(K key, V value){
         this.key = key;
         this.value = value;
     }
-
-    
+  
+   
     public K getKey() {
         return this.key;
     }
-
   
+    
     public V getValue() {
         return this.value;
     }
@@ -21,12 +24,18 @@ public class Node<K extends Comparable<K>, V> {
     public void setKey(K key){
         this.key = key;
     }
-
+  
     public void setValue(V value){
         this.value = value;
     }
-
+  
     public String toString(){
         return("\n" + "Key: " + getKey() + " Value: " + getValue());
     }
-}
+
+    @Override
+    public int compare(Node<K, V> o1, Node<K, V> o2) {
+        return o1.key.compareTo(o2.key);
+    }
+  }
+
